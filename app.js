8b8ca720208334b41009ad1e3ee01f279ebec1ae
CC-KEH <arbashhussain08@gmail.com>
2023-06-13 15:39:56 +0530

    Connection to mongodb using mongoose

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+//Connecting to mongodb using mongoose
+const promise = mongoose.connect("mongodb://localhost:27017/posts");
+
+promise.then(() => {
+  console.log("Connected to Mongo Successfully!");
+}).catch((error) => {
+  console.log(error);
+});
+
+//* Format of the database
+const postSchema = mongoose.Schema({
+    title: String,
+    post: String
+});
+
+//* Creating the Collection that follows Schema ie postSchema
+const Post = mongoose.model('Post',postSchema);
+
+
 
 const homestarter = "Lorem ipsum, dolor sit amet consectetur adipisicing asjkdaskljdh Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed labore nihil non voluptatibus adipisci quam molestiae ex explicabo natus nam illum enim, odit, ratione debitis culpa, cupiditate obcaecati corrupti architecto aliquid. Mollitia debitis itaque aliquam, nam quidem minus blanditiis nesciunt quasi voluptas, vel perspiciatis praesentium facilis expedita ad, voluptatum atque?";
 let posts = [];
